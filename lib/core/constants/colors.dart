d2602e562067f4b267e1a763e4c7295cb2b71503
QDLu <quangdunglu159@gmail.com>
2023-02-11 16:15:51 +0700

    Update app icon

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color primary = Color(0xffF4BC4B);
+const Color greyScale = Color(0xff868686);
+const Color greyDark = Color(0xff2D2E32);
+const Color blackPrimary = Color(0xff333333);
+const Color yellowLight = Color(0xffFFE9BC);
+const Color orangeSecondary = Color(0xffEE7B50);
