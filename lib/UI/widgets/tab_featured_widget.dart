7601254e8cc14e93ee69467e63d3f586a2ea8996
QDLu <quangdunglu159@gmail.com>
2023-02-12 16:58:55 +0700

    Main screen

--- lib/UI/widgets/tab_featured_widget.dart
+++ lib/UI/widgets/tab_featured_widget.dart
@@ -36,65 +36,94 @@ class _TabFeaturedWidgetState extends State<TabFeaturedWidget> {
             ),
           ),
         ),
-        Stack(
-          children: [
-            Image.asset(AssetHelper.panelHome1),
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: ClipRRect(
-                // Clip it cleanly.
-                child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                  child: Container(
-                    // width: MediaQuery.of(context).size.width,
-                    color: Colors.grey.withOpacity(0.1),
-                    alignment: Alignment.center,
-                    height: 50,
-                    child: Container(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: AppDimension.contentPadding,
+        renderTopPanel(),
+        SizedBox(
+          height: 24,
+        ),
+        Text(
+          'Summer Sale',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          'Up to 50% off with code: SS50',
+          style: TextStyle(
+            fontSize: 16,
+          ),
+        ),
+        SizedBox(
+          height: 12,
+        ),
+        Text(
+          'Selected marked products excluded from promo',
+          style: TextStyle(
+            fontSize: 12,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget renderTopPanel() {
+    return Stack(
+      children: [
+        Image.asset(AssetHelper.panelHome1),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: ClipRRect(
+            // Clip it cleanly.
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+              child: Container(
+                // width: MediaQuery.of(context).size.width,
+                color: Colors.grey.withOpacity(0.1),
+                alignment: Alignment.center,
+                height: 50,
+                child: Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: AppDimension.contentPadding,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Under Amour',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Under Amour',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
+                      GestureDetector(
+                        onTap: () {},
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Shop now',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            Icon(
+                              Icons.chevron_right,
+                              size: 21,
                               color: Colors.white,
-                            ),
-                          ),
-                          GestureDetector(
-                            onTap: () {},
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text(
-                                  'Shop now',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                Icon(
-                                  Icons.chevron_right,
-                                  size: 21,
-                                  color: Colors.white,
-                                )
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
                   ),
                 ),
               ),
             ),
-          ],
-        )
+          ),
+        ),
       ],
     );
   }
