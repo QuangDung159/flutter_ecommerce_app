d1237ed963ef966ca10f8913a3c69abf63fae212
QDLu <quangdunglu159@gmail.com>
2023-02-13 21:43:45 +0700

    Filter screen

---
+++ lib/UI/screens/filter_screen.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class FilterScreen extends StatefulWidget {
+  const FilterScreen({super.key});
+
+  @override
+  State<FilterScreen> createState() => _FilterScreenState();
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Filter'),
+    );
+  }
+}
