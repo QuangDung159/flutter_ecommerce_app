9715d8a20f4f865b734a33d5454acfa52bd229e4
QDLu <quangdunglu159@gmail.com>
2023-02-16 15:03:52 +0700

    Setup build

--- lib/UI/screens/product_detail_screen.dart
+++ lib/UI/screens/product_detail_screen.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_app/UI/widgets/cart_icon.dart';
 import 'package:flutter_ecommerce_app/core/constants/app_colors.dart';
@@ -7,6 +9,7 @@ import 'package:flutter_ecommerce_app/core/constants/app_dimension.dart';
 import 'package:flutter_ecommerce_app/core/data/product_model.dart';
 import 'package:flutter_ecommerce_app/core/helpers/asset_helper.dart';
 import 'package:get/get.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class ProductDetailScreen extends StatefulWidget {
   const ProductDetailScreen({
@@ -21,6 +24,103 @@ class ProductDetailScreen extends StatefulWidget {
 }
 
 class _ProductDetailScreenState extends State<ProductDetailScreen> {
+  final PageController _pageController = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Widget renderDotIndicator(BuildContext context) {
+    return Positioned(
+      bottom: 7,
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SmoothPageIndicator(
+              controller: _pageController,
+              count: 3,
+              effect: ExpandingDotsEffect(
+                dotWidth: 4,
+                dotHeight: 4,
+                activeDotColor: AppColors.primary,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget renderTopBar(double statusBarHeight) {
+    return Positioned(
+      top: statusBarHeight,
+      left: 0,
+      right: 0,
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: AppDimension.contentPadding,
+        ),
+        // color: Colors.amber,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            GestureDetector(
+              onTap: () => Get.back(),
+              child: Container(
+                width: 34,
+                height: 34,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(18),
+                  color: AppColors.greyDark.withOpacity(0.6),
+                ),
+                child: Image.asset(
+                  AssetHelper.iconChevronLeft,
+                  width: 8,
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Container(
+                  width: 34,
+                  height: 34,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(18),
+                    color: AppColors.greyDark.withOpacity(0.6),
+                  ),
+                  child: Image.asset(
+                    AssetHelper.iconSearch,
+                    width: 14,
+                  ),
+                ),
+                SizedBox(
+                  width: 6,
+                ),
+                Container(
+                  width: 34,
+                  height: 34,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(18),
+                    color: AppColors.greyDark.withOpacity(0.6),
+                  ),
+                  child: CartIcon(
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     double statusBarHeight = MediaQuery.of(context).padding.top;
@@ -31,73 +131,26 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
         children: [
           Stack(
             children: [
-              Image.network(
-                product.productImages[0].url,
-              ),
-              Positioned(
-                top: statusBarHeight,
-                left: 0,
-                right: 0,
-                child: Container(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: AppDimension.contentPadding,
-                  ),
-                  // color: Colors.amber,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      GestureDetector(
-                        onTap: () => Get.back(),
-                        child: Container(
-                          width: 34,
-                          height: 34,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(18),
-                            color: AppColors.greyDark.withOpacity(0.6),
-                          ),
-                          child: Image.asset(
-                            AssetHelper.iconChevronLeft,
-                            width: 8,
-                          ),
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          Container(
-                            width: 34,
-                            height: 34,
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(18),
-                              color: AppColors.greyDark.withOpacity(0.6),
-                            ),
-                            child: Image.asset(
-                              AssetHelper.iconSearch,
-                              width: 14,
-                            ),
-                          ),
-                          SizedBox(
-                            width: 6,
-                          ),
-                          Container(
-                            width: 34,
-                            height: 34,
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(18),
-                              color: AppColors.greyDark.withOpacity(0.6),
-                            ),
-                            child: CartIcon(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.width,
+                child: PageView(
+                  controller: _pageController,
+                  children: [
+                    Image.network(
+                      product.productImages[0].url,
+                    ),
+                    Image.network(
+                      product.productImages[1].url,
+                    ),
+                    Image.network(
+                      product.productImages[2].url,
+                    ),
+                  ],
                 ),
-              )
+              ),
+              renderDotIndicator(context),
+              renderTopBar(statusBarHeight)
             ],
           )
         ],
