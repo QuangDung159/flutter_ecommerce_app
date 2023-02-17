e6e885b39b54d2db5c61cdb500281b04cefa9180
QDLu <quangdunglu159@gmail.com>
2023-02-17 10:48:13 +0700

    Cart screen

--- lib/UI/screens/cart_screen.dart
+++ lib/UI/screens/cart_screen.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_app/UI/widgets/app_bar.dart';
 import 'package:flutter_ecommerce_app/UI/widgets/bottom_button.dart';
+import 'package:flutter_ecommerce_app/UI/widgets/cart_item.dart';
 import 'package:flutter_ecommerce_app/UI/widgets/filter_item_level_1.dart';
 import 'package:flutter_ecommerce_app/core/constants/app_colors.dart';
 import 'package:flutter_ecommerce_app/core/constants/app_dimension.dart';
@@ -54,153 +55,19 @@ class _CartScreenState extends State<CartScreen> {
             ),
             Expanded(
               child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Container(
-                      color: Colors.white,
-                      margin: EdgeInsets.only(top: 12),
-                      padding: EdgeInsets.only(
-                        right: AppDimension.contentPadding,
-                        left: AppDimension.contentPadding / 2,
-                        top: 12,
-                        bottom: 12,
-                      ),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Column(
-                            children: [
-                              SizedBox(
-                                height: 24,
-                              ),
-                              Image.asset(
-                                AssetHelper.iconUncheck,
-                                width: 18,
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            width: AppDimension.contentPadding / 2,
-                          ),
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(6),
-                            child: Image.network(
-                              listCartItemDummy[1].product.thumbnail,
-                              width: 90,
-                              height: 90,
-                            ),
-                          ),
-                          SizedBox(
-                            width: 14,
-                          ),
-                          Expanded(
-                            child: Column(
-                              children: [
-                                Text(
-                                  listCartItemDummy[1].product.name,
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 12,
-                                ),
-                                Container(
-                                  height: 24,
-                                  padding: EdgeInsets.symmetric(
-                                    horizontal: 10,
-                                  ),
-                                  decoration: BoxDecoration(
-                                    color: AppColors.greyDisable,
-                                    borderRadius: BorderRadius.circular(4),
-                                  ),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text(
-                                        'Green, Size 7',
-                                        style: TextStyle(
-                                          color: AppColors.grey,
-                                          fontSize: 12,
-                                        ),
-                                      ),
-                                      Image.asset(
-                                        AssetHelper.iconChevronDown,
-                                        width: 10,
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                SizedBox(
-                                  height: 12,
-                                ),
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Flexible(
-                                      flex: 55,
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(
-                                            '\$99999.99',
-                                            style: TextStyle(
-                                              fontSize: 16,
-                                              fontWeight: FontWeight.bold,
-                                              color: AppColors.primary,
-                                            ),
-                                          ),
-                                          Text(
-                                            '\$99999.99',
-                                            style: TextStyle(
-                                              fontSize: 13,
-                                              color: AppColors.greyMid,
-                                              decoration:
-                                                  TextDecoration.lineThrough,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      width: 10,
-                                    ),
-                                    Flexible(
-                                      flex: 45,
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.spaceBetween,
-                                        children: [
-                                          Image.asset(
-                                            AssetHelper.iconDecreaseCart,
-                                            width: 30,
-                                          ),
-                                          Text(
-                                            '1',
-                                            style: TextStyle(
-                                              fontSize: 16,
-                                              color: AppColors.grey,
-                                            ),
-                                          ),
-                                          Image.asset(
-                                            AssetHelper.iconIncreaseCart,
-                                            width: 30,
-                                          ),
-                                        ],
-                                      ),
-                                    )
-                                  ],
-                                )
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+                child: Container(
+                  margin: EdgeInsets.only(
+                    bottom: 12,
+                  ),
+                  child: Column(
+                    children: [
+                      CartItem(cartItem: listCartItemDummy[1]),
+                      CartItem(cartItem: listCartItemDummy[0]),
+                      CartItem(cartItem: listCartItemDummy[2]),
+                      CartItem(cartItem: listCartItemDummy[3]),
+                      CartItem(cartItem: listCartItemDummy[4]),
+                    ],
+                  ),
                 ),
               ),
             ),
---
+++ lib/UI/widgets/cart_item.dart
@@ -0,0 +1,164 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_app/core/constants/app_colors.dart';
+import 'package:flutter_ecommerce_app/core/constants/app_dimension.dart';
+import 'package:flutter_ecommerce_app/core/data/cart_item_model.dart';
+import 'package:flutter_ecommerce_app/core/helpers/asset_helper.dart';
+
+class CartItem extends StatefulWidget {
+  const CartItem({
+    Key? key,
+    required this.cartItem,
+  }) : super(key: key);
+
+  final CartItemModel cartItem;
+
+  @override
+  State<CartItem> createState() => _CartItemState();
+}
+
+class _CartItemState extends State<CartItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.only(top: 12),
+      padding: EdgeInsets.only(
+        right: AppDimension.contentPadding,
+        left: AppDimension.contentPadding / 2,
+        top: 12,
+        bottom: 12,
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            children: [
+              SizedBox(
+                height: 24,
+              ),
+              Image.asset(
+                AssetHelper.iconUncheck,
+                width: 18,
+              ),
+            ],
+          ),
+          SizedBox(
+            width: AppDimension.contentPadding / 2,
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Image.network(
+              widget.cartItem.product.thumbnail,
+              width: 90,
+              height: 90,
+            ),
+          ),
+          SizedBox(
+            width: 14,
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Text(
+                  widget.cartItem.product.name,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Container(
+                  height: 24,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 10,
+                  ),
+                  decoration: BoxDecoration(
+                    color: AppColors.greyDisable,
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Green, Size 7',
+                        style: TextStyle(
+                          color: AppColors.grey,
+                          fontSize: 12,
+                        ),
+                      ),
+                      Image.asset(
+                        AssetHelper.iconChevronDown,
+                        width: 10,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Flexible(
+                      flex: 55,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '\$99999.99',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: AppColors.primary,
+                            ),
+                          ),
+                          Text(
+                            '\$99999.99',
+                            style: TextStyle(
+                              fontSize: 13,
+                              color: AppColors.greyMid,
+                              decoration: TextDecoration.lineThrough,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Flexible(
+                      flex: 45,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Image.asset(
+                            AssetHelper.iconDecreaseCart,
+                            width: 30,
+                          ),
+                          Text(
+                            '1',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: AppColors.grey,
+                            ),
+                          ),
+                          Image.asset(
+                            AssetHelper.iconIncreaseCart,
+                            width: 30,
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
