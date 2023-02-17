f6c4cd527c988711c72ca53e9e58ed3ee7482fce
QDLu <quangdunglu159@gmail.com>
2023-02-17 09:59:20 +0700

    Cart screen

--- lib/UI/screens/sample_screen.dart
+++ lib/UI/screens/sample_screen.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_app/UI/widgets/app_bar.dart';
 import 'package:flutter_ecommerce_app/UI/widgets/bottom_button.dart';
 import 'package:flutter_ecommerce_app/UI/widgets/filter_item_level_1.dart';
-import 'package:flutter_ecommerce_app/core/constants/app_colors.dart';
 import 'package:flutter_ecommerce_app/core/constants/app_dimension.dart';
 import 'package:flutter_ecommerce_app/core/controllers/getx_app_controller.dart';
 import 'package:flutter_ecommerce_app/core/data/filter_item_model.dart';
@@ -38,51 +37,99 @@ class _SampleScreenState extends State<SampleScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
+      // backgroundColor: Colors.white,
+      body: SizedBox(
         child: Column(
           children: [
+            Container(
+              color: Colors.white,
+              height: MediaQuery.of(context).padding.top,
+            ),
             MyAppBar(
               hasBackButton: true,
-              title: 'Filter',
-              action: GestureDetector(
-                onTap: () {
-                  getxAppController.setData(listFilterSelected: []);
-                },
-                child: Text(
-                  'Reset all',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: AppColors.orangeSecondary,
-                  ),
-                ),
-              ),
+              title: 'My cart',
             ),
             Expanded(
               child: SingleChildScrollView(
                 child: Column(
                   children: [
-                    SizedBox(
-                      height: 30,
-                    ),
                     Container(
+                      color: Colors.white,
+                      margin: EdgeInsets.only(bottom: 12),
+                      height: 120,
                       padding: EdgeInsets.symmetric(
                         horizontal: AppDimension.contentPadding,
                       ),
-                      child: Obx(
-                        () => Column(
-                          children: renderListFilterLevel1(),
-                        ),
+                      child: Text('asd'),
+                    ),
+                    Container(
+                      color: Colors.white,
+                      margin: EdgeInsets.only(bottom: 12),
+                      height: 120,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: AppDimension.contentPadding,
                       ),
+                      child: Text('asd'),
+                    ),
+                    Container(
+                      color: Colors.white,
+                      margin: EdgeInsets.only(bottom: 12),
+                      height: 120,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: AppDimension.contentPadding,
+                      ),
+                      child: Text('asd'),
+                    ),
+                    Container(
+                      color: Colors.white,
+                      margin: EdgeInsets.only(bottom: 12),
+                      height: 120,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: AppDimension.contentPadding,
+                      ),
+                      child: Text('asd'),
+                    ),
+                    Container(
+                      color: Colors.white,
+                      margin: EdgeInsets.only(bottom: 12),
+                      height: 120,
+                      padding: EdgeInsets.symmetric(
+                        horizontal: AppDimension.contentPadding,
+                      ),
+                      child: Text('asd'),
                     ),
                   ],
                 ),
               ),
             ),
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.symmetric(
+                horizontal: AppDimension.contentPadding,
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('asd'),
+                      Text('asd'),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('asd'),
+                      Text('asd'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
             BottomButton(
-              title: 'Search',
+              title: 'Checkout',
               onTap: () {},
-            )
+            ),
           ],
         ),
       ),
