77a99252d85337d8e76d64ab5447781b4ef367b8
QDLu <quangdunglu159@gmail.com>
2023-02-20 23:10:29 +0700

    order screen

--- lib/UI/widgets/list_order.dart
+++ lib/UI/widgets/list_order.dart
@@ -1,7 +1,20 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_app/UI/widgets/order_item.dart';
+import 'package:flutter_ecommerce_app/core/constants/app_colors.dart';
+import 'package:flutter_ecommerce_app/core/constants/app_dimension.dart';
+import 'package:flutter_ecommerce_app/core/data/order_model.dart';
+import 'package:flutter_ecommerce_app/core/helpers/asset_helper.dart';
+import 'package:flutter_ecommerce_app/core/helpers/common_helper.dart';
 
 class ListOrder extends StatefulWidget {
-  const ListOrder({super.key});
+  const ListOrder({
+    super.key,
+    required this.listOrderModel,
+  });
+
+  final List<OrderModel> listOrderModel;
 
   @override
   State<ListOrder> createState() => _ListOrderState();
@@ -10,6 +23,157 @@ class ListOrder extends StatefulWidget {
 class _ListOrderState extends State<ListOrder> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          vertical: AppDimension.contentPadding / 2,
+        ),
+        child: Column(
+          children: renderListOrder(),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> renderListOrder() {
+    List<Widget> listRendered = [];
+
+    for (var i = 0; i < widget.listOrderModel.length; i++) {
+      listRendered.add(
+        renderOrder(
+          orderModel: widget.listOrderModel[i],
+        ),
+      );
+    }
+
+    return listRendered;
+  }
+
+  Widget renderOrder({
+    required OrderModel orderModel,
+  }) {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.only(bottom: 12),
+      child: Column(
+        children: [
+          if (orderModel.deliveryAt != null && orderModel.deliveryAt != '')
+            renderDeliveredAt(orderModel),
+          Column(
+            children: renderListOrderItem(orderModel),
+          ),
+          Container(
+            padding: EdgeInsets.only(
+              left: AppDimension.contentPadding,
+              right: AppDimension.contentPadding,
+              bottom: AppDimension.contentPadding,
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                SizedBox(
+                  width: 72,
+                ),
+                Expanded(
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        AssetHelper.iconVisa,
+                        height: 14,
+                        width: 27,
+                      ),
+                      SizedBox(
+                        width: 2,
+                      ),
+                      Text(
+                        '4242',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Total ${orderModel.orderItems.length} items',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: AppColors.grey,
+                      ),
+                    ),
+                    Text(
+                      formatPrice('200.0'),
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.primary,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  List<Widget> renderListOrderItem(OrderModel orderModel) {
+    List<Widget> listRendered = [];
+
+    for (var i = 0; i < orderModel.orderItems.length; i++) {
+      listRendered.add(
+        OrderItem(
+          orderItemModel: orderModel.orderItems[i],
+          isEndItem: i == orderModel.orderItems.length - 1,
+        ),
+      );
+    }
+
+    return listRendered;
+  }
+
+  Widget renderDeliveredAt(OrderModel orderModel) {
+    return Container(
+      margin: EdgeInsets.only(
+        top: AppDimension.contentPadding,
+        left: AppDimension.contentPadding,
+        right: AppDimension.contentPadding,
+      ),
+      height: 32,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(6),
+        border: Border.all(
+          color: AppColors.border,
+          width: 1,
+        ),
+      ),
+      child: Row(
+        children: [
+          SizedBox(
+            width: 8,
+          ),
+          Image.asset(
+            AssetHelper.iconDelivery,
+            width: 16,
+            height: 16,
+          ),
+          SizedBox(
+            width: 5,
+          ),
+          Text(
+            '${orderModel.deliveryAt} - Your package has been delivered',
+            style: TextStyle(
+              fontSize: 10,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
