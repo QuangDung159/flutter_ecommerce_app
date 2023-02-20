2bfe778020a3c0674ca81062dc5652c0a986368c
QDLu <quangdunglu159@gmail.com>
2023-02-20 22:21:26 +0700

    order screen

--- lib/UI/screens/list_order_screen.dart
+++ lib/UI/screens/list_order_screen.dart
@@ -7,7 +7,9 @@ import 'package:flutter_ecommerce_app/core/constants/app_colors.dart';
 import 'package:flutter_ecommerce_app/core/constants/app_dimension.dart';
 import 'package:flutter_ecommerce_app/core/constants/commons.dart';
 import 'package:flutter_ecommerce_app/core/controllers/getx_app_controller.dart';
+import 'package:flutter_ecommerce_app/core/data/order_model.dart';
 import 'package:flutter_ecommerce_app/core/helpers/asset_helper.dart';
+import 'package:flutter_ecommerce_app/core/helpers/common_helper.dart';
 import 'package:get/get.dart';
 
 class ListOrderScreen extends StatefulWidget {
@@ -37,85 +39,128 @@ class _ListOrderScreenState extends State<ListOrderScreen> {
             ),
             Expanded(
               child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Container(
-                      color: Colors.white,
-                      margin: EdgeInsets.only(bottom: 12),
-                      child: Column(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(
-                              top: AppDimension.contentPadding,
-                              left: AppDimension.contentPadding,
-                              right: AppDimension.contentPadding,
-                            ),
-                            height: 32,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(6),
-                              border: Border.all(
-                                color: AppColors.border,
-                                width: 1,
-                              ),
-                            ),
-                          ),
-                          Column(
-                            children: [
-                              OrderItem(
-                                orderItemModel: listOrderDummy[0].orderItems[0],
-                                isEndItem: false,
-                              ),
-                              OrderItem(
-                                orderItemModel: listOrderDummy[0].orderItems[1],
-                                isEndItem: true,
-                              ),
-                            ],
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(
-                              left: AppDimension.contentPadding,
-                              right: AppDimension.contentPadding,
-                              bottom: AppDimension.contentPadding,
-                            ),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.end,
-                              children: [
-                                SizedBox(
-                                  width: 72,
-                                ),
-                                Expanded(
-                                  child: Row(
-                                    children: [
-                                      Image.asset(
-                                        AssetHelper.iconVisa,
-                                        height: 14,
-                                        width: 27,
-                                      ),
-                                      SizedBox(
-                                        width: 2,
-                                      ),
-                                      Text('data'),
-                                    ],
-                                  ),
-                                ),
-                                Column(
-                                  children: [
-                                    Text('asd'),
-                                    Text('asd'),
-                                  ],
-                                )
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
+                child: Container(
+                  padding: EdgeInsets.symmetric(
+                    vertical: AppDimension.contentPadding / 2,
+                  ),
+                  child: Column(
+                    children: renderListOrder(),
+                  ),
                 ),
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  List<Widget> renderListOrder() {
+    List<Widget> listRendered = [];
+
+    for (var i = 0; i < listOrderDummy.length; i++) {
+      listRendered.add(
+        renderOrder(
+          orderModel: listOrderDummy[i],
+        ),
+      );
+    }
+
+    return listRendered;
+  }
+
+  Widget renderOrder({
+    required OrderModel orderModel,
+  }) {
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.only(bottom: 12),
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(
+              top: AppDimension.contentPadding,
+              left: AppDimension.contentPadding,
+              right: AppDimension.contentPadding,
+            ),
+            height: 32,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(6),
+              border: Border.all(
+                color: AppColors.border,
+                width: 1,
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              OrderItem(
+                orderItemModel: orderModel.orderItems[0],
+                isEndItem: false,
+              ),
+              OrderItem(
+                orderItemModel: orderModel.orderItems[1],
+                isEndItem: true,
+              ),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.only(
+              left: AppDimension.contentPadding,
+              right: AppDimension.contentPadding,
+              bottom: AppDimension.contentPadding,
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                SizedBox(
+                  width: 72,
+                ),
+                Expanded(
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        AssetHelper.iconVisa,
+                        height: 14,
+                        width: 27,
+                      ),
+                      SizedBox(
+                        width: 2,
+                      ),
+                      Text(
+                        '4242',
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      'Total 2 items',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: AppColors.grey,
+                      ),
+                    ),
+                    Text(
+                      formatPrice('200.0'),
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.primary,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
