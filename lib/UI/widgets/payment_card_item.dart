5899b3ae36b6ce53c0f602b4f72a414cb0cee8be
QDLu <quangdunglu159@gmail.com>
2023-02-28 09:37:04 +0700

    Card payment

--- lib/UI/widgets/payment_card_item.dart
+++ lib/UI/widgets/payment_card_item.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_app/core/constants/app_colors.dart';
 import 'package:flutter_ecommerce_app/core/constants/app_dimension.dart';
 import 'package:flutter_ecommerce_app/core/helpers/asset_helper.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class PaymentCardItem extends StatefulWidget {
   const PaymentCardItem({
@@ -20,63 +21,78 @@ class _PaymentCardItemState extends State<PaymentCardItem> {
     return Container(
       margin: EdgeInsets.only(top: 12),
       color: Colors.white,
-      child: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: AppDimension.contentPadding,
-          vertical: 12,
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: Slidable(
+        // Specify a key if the Slidable is dismissible.
+        // key: ValueKey(widget.cartItem.id),
+        endActionPane: ActionPane(
+          extentRatio: 64 / MediaQuery.of(context).size.width,
+          openThreshold: 0.1,
+          closeThreshold: 0.8,
+          // dismissible: DismissiblePane(onDismissed: () {}),
+          motion: BehindMotion(),
           children: [
-            Row(
-              children: [
-                Image.asset(
-                  AssetHelper.iconVisa,
-                  width: 48,
-                  height: 48,
-                ),
-                SizedBox(
-                  width: 12,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '**** **** **** 4242',
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      'Visa',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    )
-                  ],
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Text(
-                  'Default',
-                  style: TextStyle(
-                    color: AppColors.primary,
-                    fontWeight: FontWeight.bold,
+            Expanded(
+              child: GestureDetector(
+                onTap: () {},
+                child: Container(
+                  alignment: Alignment.center,
+                  color: AppColors.orangeSecondary,
+                  child: Image.asset(
+                    AssetHelper.iconTrash,
+                    width: 24,
                   ),
                 ),
-                SizedBox(
-                  width: 12,
-                ),
-                Icon(
-                  Icons.close,
-                  size: 20,
-                )
-              ],
-            )
+              ),
+            ),
           ],
+        ),
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: AppDimension.contentPadding,
+            vertical: 12,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Image.asset(
+                    AssetHelper.iconVisa,
+                    width: 48,
+                    height: 48,
+                  ),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '**** **** **** 4242',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Visa',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              Text(
+                'Default',
+                style: TextStyle(
+                  color: AppColors.primary,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
