ef1af7f31e554ba6e9fa80732c9283a7a9938646
QDLu <quangdunglu159@gmail.com>
2023-03-01 10:50:06 +0700

    Card payment

--- lib/core/data/user_model.dart
+++ lib/core/data/user_model.dart
@@ -1,10 +1,12 @@
 class UserModel {
   final String email, displayName, photoUrl, id;
+  final String? clientId;
 
   UserModel({
     required this.email,
     required this.displayName,
     required this.photoUrl,
     required this.id,
+    this.clientId,
   });
 }
